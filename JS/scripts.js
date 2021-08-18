6fb5ad722b3962e880c0a8ed0335663a254ed5f7
Positron11 <aarushbeleren11@gmail.com>
2021-08-18 19:06:01 +0530

    ADD LANDING PAGE AND AUDIO

---
+++ JS/scripts.js
@@ -0,0 +1,37 @@
+$(function () {
+	$("#welcome_banner").on("click", function () {
+		$("#main").css("opacity", "1");
+		$("body").css("overflow", "auto");
+		$("#welcome_overlay").css("opacity", "0");
+		$("#welcome_overlay").css("margin-left", getScrollBarWidth() + "px");
+		$("#welcome_overlay").css("pointer-events", "none");
+		document.getElementById("track").play();
+		setTimeout(function () { $("#welcome_overlay").remove(); }, 1000);
+	});
+});
+
+function getScrollBarWidth() {
+	var inner = document.createElement('p');
+	inner.style.width = "100%";
+	inner.style.height = "200px";
+
+	var outer = document.createElement('div');
+	outer.style.position = "absolute";
+	outer.style.top = "0px";
+	outer.style.left = "0px";
+	outer.style.visibility = "hidden";
+	outer.style.width = "200px";
+	outer.style.height = "150px";
+	outer.style.overflow = "hidden";
+	outer.appendChild(inner);
+
+	document.body.appendChild(outer);
+	var w1 = inner.offsetWidth;
+	outer.style.overflow = 'scroll';
+	var w2 = inner.offsetWidth;
+	if (w1 == w2) w2 = outer.clientWidth;
+
+	document.body.removeChild(outer);
+
+	return (w1 - w2);
+};
